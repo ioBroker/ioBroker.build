67b265ea381237dcd5204602909096207b125359
GermanBluefox <dogafox@gmail.com>
2015-01-02 13:22:46 +0100

    (bluefox) update utils.js for all adapters.

---
+++ adapters/utils.js
@@ -0,0 +1,34 @@
+var controllerDir;
+function getControllerDir () {
+    var fs = require('fs');
+    // Find the js-controller location
+    var controllerDir = __dirname.replace(/\\/g, '/');
+    controllerDir = controllerDir.split('/');
+    if (controllerDir[controllerDir.length - 3] == 'adapter') {
+        controllerDir.splice(controllerDir.length - 2, 2);
+        controllerDir = controllerDir.join('/');
+    } else if (controllerDir[controllerDir.length - 3] == 'node_modules') {
+        controllerDir.splice(controllerDir.length - 3, 3);
+        controllerDir = controllerDir.join('/');
+        if (fs.existsSync(controllerDir + '/iobroker.js-controller')) {
+            controllerDir += '/iobroker.js-controller';
+        } else if (fs.existsSync(controllerDir + '/controller.js')) {
+
+        } else {
+            console.log('Cannot find js-controller');
+            process.exit(10);
+        }
+    } else {
+        console.log('Cannot find js-controller');
+        process.exit(10);
+    }
+    return controllerDir;
+}
+
+function getConfig () {
+    return JSON.parse(fs.readFileSync(controllerDir + '/conf/iobroker.json'));
+}
+controllerDir = getControllerDir();
+
+exports.controllerDir = controllerDir;
+exports.getConfig =     getConfig;
