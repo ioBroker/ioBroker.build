6f87f0a670956737b1251366e828e69110450a67
GermanBluefox <dogafox@gmail.com>
2022-08-23 09:49:57 +0200

    1.1.0

--- gulpfile.js
+++ gulpfile.js
@@ -19,16 +19,16 @@ function download(url, file) {
 }
 
 gulp.task('0-clean', () => del(['tmp/**/*', 'build/.windows-ready/**/*', 'delivery/**/*']));
-gulp.task('1-nodex86', () => download(nodejsLink86, __dirname + '/build/windows/nodejs/node.msi'));
-gulp.task('2-nodex64', () => download(nodejsLink64, __dirname + '/build/windows/nodejs/node-x64.msi'));
+gulp.task('1-nodex86', () => download(nodejsLink86, `${__dirname}/build/windows/nodejs/node.msi`));
+gulp.task('2-nodex64', () => download(nodejsLink64, `${__dirname}/build/windows/nodejs/node-x64.msi`));
 gulp.task('3-0-replaceWindowsVersion', async () => {
     await checkFiles(['build/windows/nodejs/node.msi', 'build/windows/nodejs/node-x64.msi']);
 
-    !fs.existsSync(__dirname + '/build/.windows-ready') && fs.mkdirSync(__dirname + '/build/.windows-ready');
+    !fs.existsSync(`${__dirname}/build/.windows-ready`) && fs.mkdirSync(`${__dirname}/build/.windows-ready`);
 
-    let iss = fs.readFileSync(__dirname + '/build/windows/ioBroker.iss').toString('utf8');
+    let iss = fs.readFileSync(`${__dirname}/build/windows/ioBroker.iss`).toString('utf8');
     iss = iss.replace('@@version', version)
-    fs.writeFileSync(__dirname + '/build/.windows-ready/ioBroker.iss', iss);
+    fs.writeFileSync(`${__dirname}/build/.windows-ready/ioBroker.iss`, iss);
 
     fs.writeFileSync(`${__dirname}/build/.windows-ready/createSetup.bat`, `"${__dirname}/build/windows/InnoSetup6/ISCC.exe" "${__dirname}/build/.windows-ready/ioBroker.iss"`);
 });
@@ -54,7 +54,7 @@ gulp.task('3-2-copy-nodejs', async () =>
 function _checkFiles(files, callback, index) {
     index = index || 0;
 
-    if (files.find(file => !fs.existsSync(__dirname + '/' + file))) {
+    if (files.find(file => !fs.existsSync(`${__dirname}/${file}`))) {
         if (index < 5) {
             setTimeout(() => _checkFiles(files, callback, index + 1), 3000);
         } else {
@@ -114,10 +114,10 @@ function signExe() {
                 // Install node modules
                 if (process.env.CERT_FILE) {
                     fs.writeFileSync(__dirname + '/ioBrokerCodeSigningCertificate.pfx', Buffer.from(process.env.CERT_FILE, 'base64'));
-                    console.log('Saved ' + Buffer.from(process.env.CERT_FILE, 'base64').length  + ' bytes in certificate');
-                } else if (fs.existsSync(__dirname + '/ioBrokerCodeSigningCertificate.pfx') && !fs.existsSync(__dirname + '/ioBrokerCodeSigningCertificate.base64.txt')) {
-                    fs.writeFileSync(__dirname + '/ioBrokerCodeSigningCertificate.base64.txt', fs.readFileSync(__dirname + '/ioBrokerCodeSigningCertificate.pfx').toString('base64'));
-                } else if (!fs.existsSync(__dirname + '/ioBrokerCodeSigningCertificate.pfx')) {
+                    console.log(`Saved ${Buffer.from(process.env.CERT_FILE, 'base64').length} bytes in certificate`);
+                } else if (fs.existsSync(`${__dirname}/ioBrokerCodeSigningCertificate.pfx`) && !fs.existsSync(`${__dirname}/ioBrokerCodeSigningCertificate.base64.txt`)) {
+                    fs.writeFileSync(`${__dirname}/ioBrokerCodeSigningCertificate.base64.txt`, fs.readFileSync(`${__dirname}/ioBrokerCodeSigningCertificate.pfx`).toString('base64'));
+                } else if (!fs.existsSync(`${__dirname}/ioBrokerCodeSigningCertificate.pfx`)) {
                     throw new Error('NO cert file found');
                 }
 
@@ -130,7 +130,7 @@ function signExe() {
                     `/d "ioBroker windows installer" ` +
                     `/trs2 "http://timestamp.comodoca.com/?td=sha256"`;
 
-                console.log(`"${cmd.replace(process.env.CERT_PASSWORD, '*****' + ((process.env.CERT_PASSWORD || '').length + 5))}`);
+                console.log(`"${cmd.replace(process.env.CERT_PASSWORD, `*****${(process.env.CERT_PASSWORD || '').length + 5}`)}`);
 
                 // System call used for update of js-controller itself,
                 // because during installation npm packet will be deleted too, but some files must be loaded even during the installation process.
